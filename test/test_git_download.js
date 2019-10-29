5fa9c4f9888a09f5b2ce57356c4a467f311a3926
Robin <robin@naturewake.com>
2019-10-29 20:12:19 +0800

    sardine project automatic save point

--- test/test_git_download.js
+++ test/test_git_download.js
@@ -1,8 +1,18 @@
 
 const url = 'git@github.com:robin98sun/dietitian.js.git'
-const tag = 'v0.'
+const version = '0.0.30'
 const gitRootDir = './test_sardines_git_root'
 const projName = 'dietitian'
 const git = require('simple-git/promise')
 
-git(gitRootDir).clone(url)
+const fs = require('fs')
+if (!fs.existsSync(gitRootDir)) {
+  fs.mkdirSync(gitRootDir)
+}
+git(gitRootDir).clone(url).then(()=> {
+      console.log('done')
+  // git(`${gitRootDir}/dietitian.js`).fetch(url, options={'--tags': true}).then(() => {
+    // git(`${gitRootDir}/dietitian.js`).checkout(`sardines-v${version}`).then(()=> {
+    // })
+  // })
+})
